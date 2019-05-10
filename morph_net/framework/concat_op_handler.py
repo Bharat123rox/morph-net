12f4aa02ebc5f1697a0e6007fc84a6b71d9f2867
elade <elade@google.com>
2019-05-10 11:40:06 -0700

    Support tf.concat on axis different than channels (-1 or rank -1).
    PiperOrigin-RevId: 247648351

--- morph_net/framework/concat_op_handler.py
+++ morph_net/framework/concat_op_handler.py
@@ -4,8 +4,13 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from morph_net.framework import grouping_op_handler
 from morph_net.framework import op_handler
 from morph_net.framework import op_handler_util
+
+
+def _get_concat_op_axis(op):
+  return op.inputs[-1].op.get_attr('value').int_val[0]
 
 
 class ConcatOpHandler(op_handler.OpHandler):
@@ -26,6 +31,16 @@ class ConcatOpHandler(op_handler.OpHandler):
       op: tf.Operation to assign grouping to.
       op_reg_manager: OpRegularizerManager to keep track of the grouping.
     """
+    axis = _get_concat_op_axis(op)
+    # Need to figure out the rank to know if axis is last.
+    rank = len(op.inputs[0].shape)  # Rank of the first input.
+
+    if axis != -1 and axis != rank - 1:
+      # Concat is actually grouping inputs!
+      handler = grouping_op_handler.GroupingOpHandler()
+      handler.assign_grouping(op, op_reg_manager)
+      return
+
     # TODO(a1): Consider refactoring this duplicated logic.
     # Check if all input ops have groups, or tell the manager to process them.
     input_ops = op_handler_util.get_input_ops(op, op_reg_manager)
